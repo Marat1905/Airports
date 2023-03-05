09894c1e13a2c250e230f6d8d214acaca195c8e5
Marat <milya010790@mail.ru>
2023-03-05 10:57:42 +0500

    Создание Вспомогательного атрибута для десериализации

---
+++ Data/Airports.Data/Infrastructure/Attributes/CsvAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airports.Data.Infrastructure.Attributes
+{
+    // <summary>
+    /// Представляет атрибут для сериализации полей и свойств экземпляра объекта CSV.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    internal class CsvAttribute : Attribute
+    {
+        /// <summary>
+        /// Имя свойства или поля.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="CsvAttribute"/>.
+        /// </summary>
+        /// <param name="name">Имя свойства или поля.</param>
+        public CsvAttribute(string name) => Name = name;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="CsvAttribute"/>.
+        /// </summary>
+        public CsvAttribute() : this(null) { }
+    }
+}
