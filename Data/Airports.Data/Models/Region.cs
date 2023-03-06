78cd761aa167d52c251904f13444049e9fba1ffc
Marat <milya010790@mail.ru>
2023-03-06 12:23:39 +0500

    Рефакторинг модели Region

--- Data/Airports.Data/Models/Region.cs
+++ Data/Airports.Data/Models/Region.cs
@@ -1,9 +1,6 @@
-﻿using Airports.Data.Infrastructure.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Airports.Data.Enums;
+using Airports.Data.Infrastructure.Attributes;
+using Airports.Data.Infrastructure.Helper;
 
 namespace Airports.Data.Models
 {
@@ -21,48 +18,48 @@ namespace Airports.Data.Models
         /// local_code с префиксом кода страны для создания глобального уникального идентификатора
         /// </summary>
         [Csv("code")]
-        public string? Code { get; set; }
+        public string Code { get; set; }
 
         /// <summary>
         /// Местный код административной единицы. Когда это возможно, это официальные стандарты ISO 3166:2 самого высокого доступного уровня, 
         /// но в некоторых случаях нашим аэропортам приходится использовать неофициальные коды. 
         /// </summary>
         [Csv("local_code")]
-        public string? LocalCode { get; set; }
+        public string LocalCode { get; set; }
 
         /// <summary>
         /// Распространенное англоязычное название административной единицы. В некоторых случаях имя на местном языке будет отображаться в поле ключевых слов для облегчения поиска.
         /// </summary>
         [Csv("name")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// Код континента, к которому относится регион. См. поле континента в airports.csv для списка кодов
         /// </summary>
         [Csv("continent")]
-        public string? Continent { get; set; }
+        public ContinentCode Continent { get; set; }
 
         /// <summary>
         /// Двух символьный код ISO 3166:1-alpha2 для страны, содержащей административное подразделение.
         /// </summary>
         [Csv("iso_country")]
-        public string? IsoCountry { get; set; }
+        public string IsoCountry { get; set; }
 
         /// <summary>
         /// Ссылка на статью в Википедии с описанием подразделения.
         /// </summary>
         [Csv("wikipedia_link")]
-        public string? WikipediaLink { get; set; }
+        public string WikipediaLink { get; set; }
 
         /// <summary>
         /// Список ключевых слов, разделенных запятыми, для облегчения поиска. Может включать прежние названия региона и/или название региона на других языках.
         /// </summary>
         [Csv("keywords")]
-        public string? Keywords { get; set; }
+        public string Keywords { get; set; }
 
         public override string ToString()
         {
-            return $"{Id} ; {Code} ; {LocalCode} ; {Name} ; {Continent} ; {IsoCountry} ; {WikipediaLink} ; {Keywords} ";
+            return $"{Id} ; {Code} ; {LocalCode} ; {Name} ; {Continent.ToName()} ; {IsoCountry} ; {WikipediaLink} ; {Keywords} ";
         }
     }
 }
