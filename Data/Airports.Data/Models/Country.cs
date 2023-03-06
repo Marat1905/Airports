114bee79752f9d7a1898bf9e88be1f630faef4b9
Marat <milya010790@mail.ru>
2023-03-06 12:17:41 +0500

    Добавили в модель Country  ContinentCode

--- Data/Airports.Data/Models/Country.cs
+++ Data/Airports.Data/Models/Country.cs
@@ -1,9 +1,6 @@
-﻿using Airports.Data.Infrastructure.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Airports.Data.Enums;
+using Airports.Data.Infrastructure.Attributes;
+using Airports.Data.Infrastructure.Helper;
 
 namespace Airports.Data.Models
 {
@@ -20,39 +17,39 @@ namespace Airports.Data.Models
         /// Двух символьный код страны по стандарту ISO 3166:1-alpha2 . Также используется несколько неофициальных кодов, не относящихся к ISO, например «XK» для Косово .
         /// </summary>
         [Csv("code")]
-        public string? Code { get; set; }
+        public string Code { get; set; }
 
         /// <summary>
         /// Распространенное англоязычное название страны.
         /// </summary>
         [Csv("name")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// Код континента, на котором (в основном) расположена страна.
         /// </summary>
         [Csv("continent")]
-        public string? Continent { get; set; }
+        public ContinentCode Continent { get; set; }
 
         /// <summary>
         /// Ссылка на статью в Википедии о стране.
         /// </summary>
         [Csv("wikipedia_link")]
-        public string? WikipediaLink { get; set; }
+        public string WikipediaLink { get; set; }
 
         /// <summary>
         /// Разделенный запятыми список ключевых слов/фраз для поиска, относящихся к стране.
         /// </summary>
         [Csv("keywords")]
-        public string? Keywords { get; set; }
+        public string Keywords { get; set; }
 
 
 
         public override string ToString()
         {
-            return $"{Id} ; {Code} ; {Name} ; {Continent} ; {WikipediaLink} ; {Keywords} ";
+            return $"{Id} ; {Code} ; {Name} ; {Continent.ToName()} ; {WikipediaLink} ; {Keywords} ";
         }
 
-       
+
     }
 }
