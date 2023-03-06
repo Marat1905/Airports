f3d8a531e5572625e26b3f88cc02f5ff9b887e6b
Marat <milya010790@mail.ru>
2023-03-06 10:52:35 +0500

    Проверка чтения архива

---
+++ Data/Airports.Data/ReadAirportsCsv.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airports.Data
+{
+    public class ReadAirportsCsv
+    {
+        #region Поля
+        private string zipPath;
+        #endregion
+
+        #region Свойства
+        /// <summary>Путь до архива. </summary>
+        public string ZipPath
+        {
+            get { return zipPath; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Строка не может быть пустой или null строкой или содержать пробел", nameof(value));
+                zipPath = value;
+            }
+        }
+        #endregion
+
+        #region Конструкторы
+        /// <summary>Инициализирует новый экземпляр класса <see cref="ReadAirportsCsv"/>. </summary>
+        /// <param name="zipPath">Полный путь до архива.</param>
+        public ReadAirportsCsv(string zipPath) => ZipPath = zipPath;
+        #endregion
+
+        #region Методы
+        /// <summary>Чтение архива </summary>
+        /// <returns>Выдает список файлов в архиве </returns>
+        /// <exception cref="FileNotFoundException"> Если не правильно указан путь к файлу за архивированного файла</exception>
+        public IEnumerable<ZipArchiveEntry> ReadZip()
+        {
+            List<ZipArchiveEntry> result = new List<ZipArchiveEntry>();
+            //Проверяем правильность указания пути к архиву.
+            FileInfo file = new FileInfo(ZipPath);
+            if (!file.Exists)
+                throw new FileNotFoundException("Не правильно указан путь к архиву", ZipPath);
+            //Получаем список файлов в архиве
+            using (ZipArchive archive = ZipFile.OpenRead(ZipPath))
+            {
+                result.AddRange(archive.Entries);
+            }
+            return result;
+        }
+        /// <summary>Проверяет наличие файла в архиве</summary>
+        /// <param name="fileName">Имя файла с расширением</param>
+        /// <returns></returns>
+        public bool IsFile(string fileName)
+        {
+            return ReadZip().Any(x => x.Name == fileName);
+        }
+        #endregion
+    }
+}
--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Airports.Data;
+using Airports.Data.Models;
+
+Console.Title = "MyProg";
+
+string zipPath = @"C:\Users\Marat\Downloads\Airports.zip";
+string fileCountries = "countries.csv";
+string fileRegions = "regions.csv";
+string fileAirports = "airports.csv";
+string fileAirportFrequencies = "airport-frequencies.csv";
+string fileNavaids = "navaids.csv";
+string fileRunways = "runways.csv";
+
+ReadAirportsCsv readAirports = new ReadAirportsCsv(zipPath);
+var files = readAirports.ReadZip();
+Console.WriteLine(readAirports.IsFile(" "));
+Console.WriteLine(readAirports.IsFile("regions.csv"));
 Console.WriteLine();
+foreach (var item in files)
+{
+    Console.WriteLine(item.Name);
+}
+
 
