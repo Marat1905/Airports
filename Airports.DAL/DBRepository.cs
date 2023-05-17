696c7d78c638b0b8ba32b6d4e16b8615f9f6ca60
Marat <milya010790@mail.ru>
2023-05-17 11:13:49 +0500

    Базовая реализация репозитория

---
+++ Airports.DAL/DBRepository.cs
@@ -0,0 +1,83 @@
+﻿using Airports.DAL.Context;
+using Airports.DAL.Entityes.Base;
+using Airports.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Airports.DAL
+{
+    public class DBRepository<T> : IRepository<T> where T : Entity, new()
+    {
+        private readonly AirpotsDB _db;
+        private readonly DbSet<T> _Set;
+
+        public bool AutoSaveChanges { get; set; } = true;
+
+        public DBRepository(AirpotsDB db)
+        {
+            _db = db;
+            _Set = db.Set<T>();
+        }
+
+        public IQueryable<T> Items => _Set;
+
+        public T Get(int id) => Items.SingleOrDefault(i => i.Id == id);
+
+        public async Task<T> GetAsync(int id, CancellationToken Cancel = default)=> await Items.
+            SingleOrDefaultAsync(i=>i.Id== id,Cancel).
+            ConfigureAwait(false);
+        public T Add(T item)
+        {
+            if(item is null) throw new ArgumentNullException(nameof(item));
+            _db.Entry(item).State = EntityState.Added;
+            if (AutoSaveChanges)
+                _db.SaveChanges();
+            return item;
+        }
+
+        public async Task<T> AddAsync(T item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _db.Entry(item).State = EntityState.Added;
+            if (AutoSaveChanges)
+                await _db.SaveChangesAsync(Cancel).ConfigureAwait(false);
+            return item;
+        }
+
+        public void Update(T item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _db.Entry(item).State = EntityState.Modified;
+            if (AutoSaveChanges)
+                _db.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _db.Entry(item).State = EntityState.Modified;
+            if (AutoSaveChanges)
+                await _db.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+
+        public void Remove(int id)
+        {
+            var item = _Set.Local.FirstOrDefault(i => i.Id == id) ?? new T { Id = id };
+            _db.Remove(item);
+            if (AutoSaveChanges)
+                _db.SaveChanges();
+        }
+
+        public async Task RemoveAsync(int id, CancellationToken Cancel = default)
+        {
+            _db.Remove(new T { Id = id });
+            if (AutoSaveChanges)
+                await _db.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        } 
+    }
+}
--- Airports.DAL/Entityes/Base/Entity.cs
+++ Airports.DAL/Entityes/Base/Entity.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Airports.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Airports.DAL.Entityes.Base
 {
-    public abstract class Entity
+    public abstract class Entity:IEntity
     {
         /// <summary>
         /// Внутренний целочисленный идентификатор OurAirports для навигационного средства. Это останется постоянным, даже если идентификатор навигационного средства или частота изменятся.
