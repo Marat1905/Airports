9705db12441c84c65f46c5426aa8d10108d13b8c
Marat <milya010790@mail.ru>
2023-05-22 22:01:06 +0500

    Главная модель и локатор представления

---
+++ Tests/Airports.TestWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Airports.TestWpf.ViewModels.Base;
+
+
+namespace Airports.TestWpf.ViewModels
+{
+    internal class MainWindowViewModel:ViewModel
+    {
+        #region Заголовок окна. 
+        private string _Title="Главное окно";
+        /// <summary>Заголовок окна.</summary>
+        public string Title { get => _Title ; set { Set(ref _Title, value); } }
+        #endregion
+
+
+
+    }
+}
