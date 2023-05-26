6884c443b48b69fba47e29cd38c4f27c21894f78
Marat <milya010790@mail.ru>
2023-05-26 11:20:14 +0500

    Добавил view FindAiroportView

---
+++ Tests/Airports.TestWpf/Views/FindAiroportView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Airports.TestWpf.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для FindAiroportView.xaml
+    /// </summary>
+    public partial class FindAiroportView : UserControl
+    {
+        public FindAiroportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
