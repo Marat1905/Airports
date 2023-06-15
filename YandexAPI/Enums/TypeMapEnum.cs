96e71b2046a35a0d542ea9d014351b0200548241
Marat <milya010790@mail.ru>
2023-06-15 11:20:59 +0500

    Создал мини API

---
+++ YandexAPI/Enums/TypeMapEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace YandexAPI.Enums
+{
+    /// <summary>Слои и типы карты</summary>
+    public enum TypeMapEnum 
+    {
+        /// <summary>Схема местности и названия географических объектов.</summary>
+        Map,
+        /// <summary>Местность, сфотографированная со спутника.</summary>
+        Sat,
+        /// <summary>Названия географических объектов.</summary>
+        Skl,
+        /// <summary>Слой пробок.</summary>
+        Trf
+    }
+}
---
+++ YandexAPI/Mаps/Interfaces/IStaticMaps.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Net;
+using YandexAPI.Enums;
+
+namespace YandexAPI.Mаps.Interfaces
+{
+    public interface IStaticMaps
+    {
+
+        /// <summary>Возвращает URL на статический рисунок карты с точкой поиска в центре </summary>
+        /// <param name="type">Слои и типы карт</param>
+        /// <param name="Latitude">Широта</param>
+        /// <param name="Longitude">Долгота</param>
+        /// <param name="zPosition">Уровень масштабирования. Может быть от 1 до 17</param>
+        /// <param name="width">Ширина. Может быть от 1 до 650</param>
+        /// <param name="height">Высота. Может быть от 1 до 450</param>
+        /// <returns>Url на Image</returns>
+        string GetUrlMapImage(TypeMapEnum type, double Latitude, double Longitude, int zPosition, int width, int height);
+
+        /// <summary>
+        /// Метод для скачивания Image из интернета
+        /// </summary>
+        /// <param name="URL">URL скачиваемой Image</param>
+        /// <param name="Proxy">Передаем Proxy для подключения</param>
+        /// <returns>Возвращаем объект Image</returns>
+        Bitmap DownloadMapImage(string Url, WebProxy Proxy = null);
+
+    }
+}
