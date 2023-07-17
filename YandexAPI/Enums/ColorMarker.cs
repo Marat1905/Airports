ed183adbff531cad91c24280434eaef5713fe02a
Marat <milya010790@mail.ru>
2023-07-17 15:07:37 +0500

    перечисления цвета меток

---
+++ YandexAPI/Enums/ColorMarker.cs
@@ -0,0 +1,47 @@
+﻿namespace YandexAPI.Enums
+{
+    /// <summary> Цвета меток </summary>
+    public enum ColorMarker
+    {
+        /// <summary>Белый (по умолчанию)</summary>
+        wtM,
+        /// <summary>Темно-оранжевый</summary>
+        doM,
+        /// <summary>Темно-синий</summary>
+        dbM,
+        /// <summary>Синий</summary>
+        blM,
+        /// <summary>Зеленый</summary>
+        gnM,
+        /// <summary>Серый</summary>
+        grM,
+        /// <summary>Светло-синий</summary>
+        lbM,
+        /// <summary>Темная ночь</summary>
+        ntM,
+        /// <summary>Оранжевый</summary>
+        orM,
+        /// <summary>Розовый</summary>
+        pnM,
+        /// <summary>Красный</summary>
+        rdM,
+        /// <summary>Фиолетовый</summary>
+        vvM,
+        /// <summary>Желтый</summary>
+        ywM,
+        /// <summary>С буквой "А" (без указания контента и размера)</summary>
+        a,
+        /// <summary>С буквой "Б" (без указания контента и размера)</summary>
+        b,
+        /// <summary>Голубой (без указания контента)</summary>
+        org,
+        /// <summary>Фиолетовый (без указания контента)</summary>
+        dir,
+        /// <summary>Голубой с жёлтой точкой</summary>
+        blyw,
+        /// <summary>Черная кнопка</summary>
+        bk,
+        /// <summary>Серая кнопка</summary>
+        gr
+    }
+}
