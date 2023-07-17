88838dfa4b1ed568de894b8dd2db15266ea9b7b8
Marat <milya010790@mail.ru>
2023-07-17 15:07:11 +0500

    перечисления стилей меток

---
+++ YandexAPI/Enums/StyleMarker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YandexAPI.Enums
+{
+    /// <summary> Стили меток </summary>
+    public enum StyleMarker
+    {
+        /// <summary> </summary>
+        pm,
+        /// <summary>Метка с хвостиком. Нужно составлять цвет и контент</summary>
+        pm2,
+        /// <summary>Флаг</summary>
+        flag,
+        /// <summary>Кнопка</summary>
+        vk,
+        /// <summary>Голубая метка с хвостиком</summary>
+        org,
+        /// <summary>Голубая метка с кругом в центре</summary>
+        comma,
+        /// <summary>Голубая метка в виде круга</summary>
+        round,
+        /// <summary>Значок дома</summary>
+        home,
+        /// <summary>Значок работы</summary>
+        work,
+        /// <summary>Значок с логотипом Яндекса. </summary>
+        ya_ru,
+    }
+}
