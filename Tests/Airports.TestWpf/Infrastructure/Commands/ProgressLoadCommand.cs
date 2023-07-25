32c9170cc2fa28897876fbb8cb42f49849231c68
Marat <milya010790@mail.ru>
2023-07-25 20:15:46 +0500

    Команда для закрытия окна прогресс бара

---
+++ Tests/Airports.TestWpf/Infrastructure/Commands/ProgressLoadCommand.cs
@@ -0,0 +1,32 @@
+﻿using Airports.TestWpf.Infrastructure.Commands.Base;
+using Airports.TestWpf.Views.Windows;
+using System;
+using System.Windows;
+
+namespace Airports.TestWpf.Infrastructure.Commands
+{
+    internal class ProgressLoadCommand:Command
+    {
+        private ProgressLoadWindow _Window;
+
+        public override bool CanExecute(object parameter) => _Window == null;
+
+        public override void Execute(object parameter)
+        {
+            var window = new ProgressLoadWindow
+            {
+                Owner = Application.Current.MainWindow
+            };
+            _Window = window;
+            window.Closed += OnWindowClosed;
+
+            window.ShowDialog();
+        }
+
+        private void OnWindowClosed(object Sender, EventArgs E)
+        {
+            ((Window)Sender).Closed -= OnWindowClosed;
+            _Window = null;
+        }
+    }
+}
