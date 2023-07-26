f4cb78d9943bdf168e3700974173eed27cca271a
Marat <milya010790@mail.ru>
2023-07-26 22:02:35 +0500

    Добавлен ProgressBar

---
+++ Tests/Airports.TestWpf/Model/ProgressLoadModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airports.TestWpf.Model
+{
+    public class ProgressLoadModel
+    {
+        /// <summary>Общее количество итераций</summary>
+        public int CountRows { get; set; }
+        /// <summary>Счетчик строк </summary>
+        public int ProgressCount { get; set; }
+        /// <summary>Количество загруженных файлов</summary>
+        public int CountDownloadFiles { get; set; }
+        /// <summary>Какой файл грузится </summary>
+        public string File { get; set; }
+
+        /// <summary>Для прогресс бара </summary>
+        /// <param name="countRows">Общее количество итераций</param>
+        /// <param name="progressCount">Счетчик строк</param>
+        /// <param name="countDownloadFiles">Количество загруженных файлов</param>
+        /// <param name="file">Какой файл грузится</param>
+        public ProgressLoadModel(int countRows,int progressCount,int countDownloadFiles,string file)
+        {
+            CountRows = countRows;
+            ProgressCount = progressCount;
+            CountDownloadFiles = countDownloadFiles;
+            File = file;
+        }
+    }
+}
