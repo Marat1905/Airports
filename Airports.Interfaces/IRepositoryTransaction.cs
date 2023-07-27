d13646c82f6284999ce87ef996d69b58d84dfc3a
Marat <milya010790@mail.ru>
2023-07-27 12:40:00 +0500

    Интерфейс для управления транзакциями

---
+++ Airports.Interfaces/IRepositoryTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airports.Interfaces
+{
+    /// <summary>Интерфейс управления транзакциями </summary>
+    public interface IRepositoryTransaction:IDisposable
+    {
+
+        /// <summary>Сохраняем изменения в БД</summary>
+        void Commit();
+
+        /// <summary>Откатываем все изменения в БД</summary>
+        void Rollback();
+    }
+}
